6a1128a85a5804de6e70c11c4a1d9f0350b4e4e8
Cosmo0004 <108553854+Cosmo0004@users.noreply.github.com>
2022-08-21 20:47:05 -0600

    Add files via upload

---
+++ TipoVendedor.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sc202.proyecto.controller;
+
+/**
+ *
+ * @author nicol
+ */
+public enum TipoVendedor 
+{
+    VENTANILLA,
+    INTERNET
+}
